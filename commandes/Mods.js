4dd4c1940b831aacbaa1eeb484b13865d327070d
DARK SASIYA <164819368+feenixmd@users.noreply.github.com>
2024-04-30 19:41:38 +0530

    Update Mods.js

--- commandes/Mods.js
+++ commandes/Mods.js
@@ -47,7 +47,7 @@ const sleep =  (ms) =>{
         type = 'not animated sticker'
       }
   
-      let msg = `   Dexter-stickers-dl
+      let msg = `   Feenix-stickers-dl
       
   *Name :* ${stickers.data.result.name}
   *Type :* ${type} 
